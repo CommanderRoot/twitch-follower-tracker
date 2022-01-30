61ce9cc77077d2865fb3e88a519679975b7cfa35
Tobias Speicher <rootcommander@gmail.com>
2022-01-30 09:34:25 +0100

    Switch to GQL API as Kraken is shutting down soon
    Helix would have been proper but that requires auth so it's a hassle to setup and maintain for just a simple script

--- get_follower_as_list.php
+++ get_follower_as_list.php
@@ -1,23 +1,5 @@
 <?php
 // äüö UTF8-FTW
-function convert_time_to_pacific(string $time_string): string {
-	$dateTime = new DateTime(date('c', strtotime($time_string)), new DateTimeZone(date_default_timezone_get()));
-	$dateTime->setTimezone(new DateTimeZone('America/Los_Angeles'));
-
-	$return = $dateTime->format('Y-m-d h:i:s A T');
-	return $return;
-}
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_ENCODING, '');
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:', 'Accept: application/vnd.twitchtv.v5+json', 'Client-ID: 1rr2wks0n53qby34wanxhirvlo50359'));
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-curl_setopt($ch, CURLOPT_AUTOREFERER, false);
-
-
 if(isset($argv[1]) && !empty($argv[1])) {
 	$target_user = strtolower($argv[1]);
 } else {
@@ -25,121 +7,95 @@ if(isset($argv[1]) && !empty($argv[1])) {
 	exit();
 }
 
-// Get User ID from username (login)
-$target_user_id = '';
-$request_url = 'https://api.twitch.tv/kraken/users?login='.rawurlencode($target_user);
-curl_setopt($ch, CURLOPT_URL, $request_url);
-$curl_ouput = curl_exec($ch);
-$curl_info = curl_getinfo($ch);
-if($curl_info['http_code'] == 200) {
-	$json_decode = json_decode($curl_ouput, true);
-	if($json_decode !== null) {
-		foreach($json_decode['users'] as $user) {
-			if($user['name'] == $target_user) {
-				$target_user_id = $user['_id'];
-				break;
-			}
-		}
-	}
-}
-if(empty($target_user_id)) {
-	echo 'Error while getting ID for user: '.$curl_ouput.PHP_EOL;
-	exit();
-}
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_ENCODING, '');
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Expect:', 'Content-Type: application/json', 'Client-ID: kimne78kx3ncx6brgo4mv6wki5h1ko']);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+curl_setopt($ch, CURLOPT_AUTOREFERER, false);
+curl_setopt($ch, CURLOPT_URL, 'https://gql.twitch.tv/gql');
 
-
-$show_timestamp = false;
 $write_to_file = true;
-$cursor = '';
-
-// Following
-// $request_url = 'https://api.twitch.tv/kraken/users/'.rawurlencode($target_user_id).'/follows/channels?sortby=created_at&direction=desc&limit=100&offset=0&cursor=';
-// Follower
-$request_url = 'https://api.twitch.tv/kraken/channels/'.rawurlencode($target_user_id).'/follows?direction=desc&limit=100&cursor=';
-
 if($write_to_file === true) {
 	$handle = fopen(__DIR__ . DIRECTORY_SEPARATOR . $target_user.'.txt', 'wb');
 }
 
+$cursor = '';
 $keep_running = true;
 while($keep_running === true) {
+	$gqlQuery = [[
+		'operationName' => 'Followers',
+		'variables' => [
+			'limit' => 100,
+			'login' => $target_user,
+			'order' => 'DESC',
+		],
+		'extensions' => [
+			'persistedQuery' => [
+				'version' => 1,
+				'sha256Hash' => 'deaf3a7c3227ae1bfb950a3d3a2ba8bd47a01a5b528c93ae603c20427e1d829d',
+			],
+		],
+	]];
+	if(!empty($cursor)) {
+		$gqlQuery[0]['variables']['cursor'] = $cursor;
+	}
+
 	redo_request:
-	// echo 'Request: '.$request_url.$cursor.PHP_EOL;
-	curl_setopt($ch, CURLOPT_URL, $request_url.$cursor);
+	// echo 'Requesting cursor: '.$cursor.PHP_EOL;
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($gqlQuery, JSON_UNESCAPED_UNICODE));
 	$curl_ouput = curl_exec($ch);
 	$curl_info = curl_getinfo($ch);
 	if($curl_info['http_code'] == 200) {
 		$json_decode = json_decode($curl_ouput, true);
-		if($json_decode !== NULL) {
-			if(isset($json_decode['follows']) && count($json_decode['follows']) > 0) {
-				foreach($json_decode['follows'] as $follow) {
-					if(isset($follow['user']['name'])) {
-						$string = '';
-						$string .= $follow['user']['name'];
-						if($show_timestamp === true) {
-							$string .= '	'.convert_time_to_pacific($follow['created_at']);
-						}
-						$string .= PHP_EOL;
-	
-						echo $string;
+		if($json_decode !== null) {
+			if(isset($json_decode[0], $json_decode[0]['data'], $json_decode[0]['data']['user'], $json_decode[0]['data']['user']['followers'], $json_decode[0]['data']['user']['followers']['edges']) && !is_null($json_decode[0]['data']['user']['followers']['edges']) && count($json_decode[0]['data']['user']['followers']['edges']) > 0) {
+				$string = '';
+				foreach($json_decode[0]['data']['user']['followers']['edges'] as $follow) {
+					if(isset($follow['node']['login'])) {
+						$string .= $follow['node']['login'].PHP_EOL;
+					}
 
-						if($write_to_file === true) {
-							fwrite($handle, $string);
-						}
-					} elseif(isset($follow['channel']['name'])) {
-						$string = '';
-						$string .= $follow['channel']['name'];
-						if($show_timestamp === true) {
-							$string .= '	'.convert_time_to_pacific($follow['created_at']);
-						}
-						$string .= PHP_EOL;
-
-						echo $string;
-
-						if($write_to_file === true) {
-							fwrite($handle, $string);
-						}
+					// Update cursor
+					$cursor = isset($follow['cursor']) ? $follow['cursor'] : '';
+					if(empty($cursor)) {
+						echo PHP_EOL.PHP_EOL.'Done!'.PHP_EOL;
+						$keep_running = false;
 					}
 				}
 
-				// Update cursor
-				$cursor = isset($json_decode['_cursor']) ? $json_decode['_cursor'] : '';
-				if(empty($cursor)) {
-					echo PHP_EOL.PHP_EOL.'Done!'.PHP_EOL;
-					$keep_running = false;
+				echo $string;
+
+				if($write_to_file === true && strlen($string) > 0) {
+					fwrite($handle, $string);
 				}
 			} else {
 				echo PHP_EOL.PHP_EOL.'Done!'.PHP_EOL;
 				$keep_running = false;
 			}
 		} else {
-			echo 'Json_decode Error'.PHP_EOL;
+			echo 'Json_decode error ('.json_last_error_msg().') on: '.$curl_ouput.PHP_EOL;
 			goto redo_request;
 		}
 	} elseif($curl_info['http_code'] == 502) {
-		// Server Error also retry
-		echo '502 Error'.PHP_EOL;
+		// Server error so retry
+		echo '502 error'.PHP_EOL;
 		goto redo_request;
 	} elseif($curl_info['http_code'] == 503) {
-		// Server Error also retry
-		echo '503 Error'.PHP_EOL;
+		// Server error so retry
+		echo '503 error'.PHP_EOL;
 		goto redo_request;
 	} elseif($curl_info['http_code'] == 0) {
-		// Timeout also retry
-		echo 'Timeout Error'.PHP_EOL;
+		// Timeout so retry
+		echo 'Timeout error'.PHP_EOL;
 		echo 'Curl error: '.curl_error($ch).PHP_EOL;
 		// exit();
 		goto redo_request;
-	} elseif($curl_info['http_code'] == 404) {
-		// Deleted
-		echo 'Deleted'.PHP_EOL;
-		exit();
-	} elseif($curl_info['http_code'] == 422) {
-		// TOS'd
-		echo 'TOS\'d'.PHP_EOL;
-		exit();
 	} else {
-		echo 'HTTP ERROR: '.$curl_info['http_code'].PHP_EOL;
+		echo 'HTTP error: '.$curl_info['http_code'].PHP_EOL;
 		goto redo_request;
 	}
 }
