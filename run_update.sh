6cb4c3875146e8acfcdb1f230fedfd9e1efe87cd
Tobias Speicher <rootcommander@gmail.com>
2022-02-20 04:41:11 +0100

    Add current date and time to commit message

--- run_update.sh
+++ run_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
+CURDATE=$(date +"%Y-%m-%d %T")
 
 if [ -z "$1" ]
 	then
@@ -9,4 +10,4 @@ fi
 
 php -f ${BASEDIR}/get_follower_as_list.php $1
 
-cd ${BASEDIR} && git add $1.txt && git commit -m "Update $1" && git push
+cd ${BASEDIR} && git add $1.txt && git commit -m "Update $1 at $CURDATE" && git push
